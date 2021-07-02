29bd7e1532e8be257c87b2e37cbe54c0facd1bf7
ialoig <ialoig@protonmail.com>
2021-07-02 16:18:13 +0200

    Added example & implementation

---
+++ README.md
@@ -0,0 +1,82 @@
+<div align="center">
+    <br/><em>Brought with</em> &nbsp;❤️ <em>by</em> &nbsp; ialoig
+</div>
+
+
+# Introduction
+
+<p align="center">
+  TouchableScale.js is a simple React Native component, that scale when a gesture is endled.
+</p>
+
+![Button scale](../react-native-touchable-scale/example/react-native-touchable-scale/src/assets/img/scale-button.gif)
+
+
+
+<div align="center">
+  <a href="#GettingStarted">Getting Started</a> &nbsp;|&nbsp; 
+  <a href="#Properties">Properties</a> &nbsp;|&nbsp; 
+  <a href="#Example">Example</a> &nbsp;|&nbsp; 
+</div>
+
+
+
+
+<h1 id="Getting-Started">Getting Started</h1>
+
+## Prerequisites
+* [React Native](https://facebook.github.io/react-native/docs/getting-started.html)
+* [node v14.17.1](https://github.com/creationix/nvm)
+
+
+
+<h1 id="Properties">Properties</h1>
+
+### Touchable Scale
+
+| Property                      | Type                                                  | Optional |  Default                                                                                                                                                                                       | Description                                              |
+| :-----: | :---------------------------------------------------: | :-------:| :-----------------------------------------------: | :--------------------:|
+| `style`| `array or object`|Yes| `-`| Style definition of the component we want to render|
+| `children`| `array or object`|No|`-`| Component|
+| `onPress`| `func`|Yes|`-`| Function to be called when onPress event is handled|
+| `scaleValue`| `number`|Yes|`0.95`| Set how much you want to scale the component|
+| `durationValue`|`number`|Yes| `150`| Set the scale duration|
+
+
+
+<h1 id="Example">Example</h1>
+
+You can find an example on /example/react-native-touchable-scale/.
+
+Run installation from hereAdd latest package version
+```bash
+$ npm install
+```
+
+Run app with the following command package version
+```bash
+$ npm start
+```
+
+
+There is a defined component (ie. Button ), wrapped with the Touchable Scale component:
+
+```jsx
+import React from 'react'
+import TouchableScale from "../TouchableScale"
+
+function Button ({ title, type, size, border, onPress }) {
+
+  return (
+    <TouchableScale 
+      onPress={onPress} 
+      style={[ button.button, button[type], button[size], border ? button.border : null ]]}>
+      <Text style={[ text.button, text[type]]}>
+        {title}
+      </Text>
+    </TouchableScale>
+  )
+}
+
+export default Button
+```
---
+++ src/index.js
@@ -0,0 +1,122 @@
+import PropTypes from "prop-types"
+import React from "react"
+import { Platform } from "react-native"
+import { TapGestureHandler } from "react-native-gesture-handler"
+import Animated, { 
+	Easing,
+	runOnJS,
+	useAnimatedGestureHandler,
+	useAnimatedStyle,
+	useDerivedValue,
+	useSharedValue,
+	withTiming } from "react-native-reanimated"
+
+const DEFAULT_DURATION = 150
+const DEFAULT_SCALE = 0.95
+
+
+function TouchableScale({ style, children, onPress, scaleValue, durationValue, ...props }) {
+    
+	//define scale value to set; change every time an event is catch
+	const scale = useSharedValue(1)
+	//define if press event has been released 
+	const releasePress = useSharedValue(false)
+	//duration time defined by props or by default value
+	const activeDuration = durationValue !== undefined ? durationValue : DEFAULT_DURATION
+	//active scale value defined by props or by default value
+	const activeScale = scaleValue !== undefined ? scaleValue : DEFAULT_SCALE
+
+	//value when component is pressed and scale decrease
+	const scaleIn = useDerivedValue( () => {
+		const value = withTiming(activeScale, { 
+			duration: activeDuration,
+			easing: Easing.bezier(0.25, 0.1, 0.25, 1),
+			useNativeDriver: true,
+		})
+		return value
+	})
+
+	//value when component is on starting state
+	const scaleOut = useDerivedValue( () => {
+		const value = withTiming(1, { 
+			duration: activeDuration,
+			useNativeDriver: true,
+		})
+		return value
+	})
+
+
+	const eventHandler = useAnimatedGestureHandler({
+		onStart: () => {
+			releasePress.value = true
+			scale.value = scaleIn.value
+		},
+		onEnd: () => {
+			releasePress.value = false
+			scale.value = scaleOut.value
+			if (onPress)
+				runOnJS(onPress)?.()
+		},
+		onFinish: () => {
+			releasePress.value = false
+			scale.value = scaleOut.value
+		},
+	})
+
+
+	const animatedStyle = useAnimatedStyle( () => {
+		return {
+			transform: [{ scale: onPress === undefined ? 1 : scale.value }], //disabled scale when onPress function is not defined
+		}
+	})
+
+
+	if (Platform.OS === "android") {
+		return (
+			<TapGestureHandler 
+				onGestureEvent={eventHandler}
+				// Otherwise animation stops after short time on android
+				maxDurationMs={10000000000}
+				//Sets additional distance outside of element in which a press can be detected.
+				hitSlop={5}
+				//Maximum time, expressed in milliseconds, that can pass before the next tap — if many taps are required.
+				maxDelayMs={0}
+			>
+				<Animated.View style={[style, animatedStyle]} pointerEvents="box-only">
+					{children}
+				</Animated.View>
+			</TapGestureHandler>
+		)
+	}
+	else if (Platform.OS === "ios") {
+		return (
+			<TapGestureHandler 
+				onGestureEvent={eventHandler}
+				//Maximum distance, expressed in points, that defines how far the finger is allowed to travel along the X axis during a tap gesture.
+				maxDeltaX={40} 
+				//Maximum distance, expressed in points, that defines how far the finger is allowed to travel along the Y axis during a tap gesture
+				maxDeltaY={40}
+			>
+				<Animated.View style={[style, animatedStyle]} pointerEvents="box-only" {...props}>
+					{children}
+				</Animated.View>
+			</TapGestureHandler>
+		)
+	}
+}
+
+
+TouchableScale.propTypes = {
+	style: PropTypes.oneOfType([PropTypes.array, PropTypes.object]),
+	children: PropTypes.oneOfType([PropTypes.array, PropTypes.object]).isRequired,
+	onPress: PropTypes.func,
+	scaleValue: PropTypes.number,
+	durationValue: PropTypes.number,
+}
+
+TouchableScale.defaultProps = {
+	scaleValue: DEFAULT_SCALE,
+	durationValue: DEFAULT_DURATION
+}
+
+export default TouchableScale
